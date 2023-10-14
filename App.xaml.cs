9bce8bc18992ea55f685a52402ed418ca807591e
Yoyo999111222 <106955551+Yoyo999111222@users.noreply.github.com>
2023-10-14 23:02:18 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_Application
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
